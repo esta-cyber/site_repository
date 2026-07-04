e74c304d3e40e3f94b9a076a5deaddbf17178880
firdavs <sharifasanakulova49@gmail.com>
2026-07-04 21:58:53 +0500

    i dont know

--- sayt/urls.py
+++ sayt/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from . import views
+from .views import *
 
 urlpatterns = [
-    path('', views.enter, name='enter'),
-    path('logout/', views.logout_view, name='logout'),
-    path('admin-boshqarish-bulimi/', views.admin_boshqaruv_bulimi, name='admin_bulimi'),
-    path('oqituvchi-boshqaruv-bulimi/', views.teacher_boshqaruv_bulimi, name='teacher_bulimi'),
-    path('talabalar-bulimi/', views.uquvchining_bulimi, name='uquvchi_bulimi'),
-    path('store/', views.store_page, name='store'),
-    path('chat/', views.chat_page, name='chat'),
+    path('', enter, name='enter'),
+    path('logout/', logout_view, name='logout'),
+    path('admin-boshqarish-bulimi/', admin_boshqaruv_bulimi, name='admin_bulimi'),
+    path('oqituvchi-boshqaruv-bulimi/', teacher_boshqaruv_bulimi, name='teacher_bulimi'),
+    path('talabalar-bulimi/', uquvchining_bulimi, name='uquvchi_bulimi'),
+    path('store/', store_page, name='store'),
+    path('chat/', chat_page, name='chat'),
 ]
